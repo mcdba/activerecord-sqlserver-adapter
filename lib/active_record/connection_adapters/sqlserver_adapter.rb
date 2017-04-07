9aebb675fdd68166c52b470ac67cba0e7ae7c52b
Michail Kabanov <mcdba@mail.ru>
2017-04-07 16:19:43 +0300

    fix arel

--- lib/active_record/connection_adapters/sqlserver_adapter.rb
+++ lib/active_record/connection_adapters/sqlserver_adapter.rb
@@ -1,6 +1,7 @@
 require 'base64'
 require 'active_record'
-require 'arel_sqlserver'
+require 'arel/arel_sqlserver'
+require 'arel/visitors/bind_visitor'
 require 'active_record/connection_adapters/abstract_adapter'
 require 'active_record/connection_adapters/sqlserver/core_ext/active_record'
 require 'active_record/connection_adapters/sqlserver/core_ext/explain'
